3879628ba4ce76a1a10c0a6531441cdb16fb870e
Your Name <you@example.com>
2017-10-14 14:51:23 -0400

    Initial Commit for Fall22017 repo

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Fall2-2017
-C291-Fall2017 (8W2)
+# C291 Fall1-2017
+
+This is the course repository for C291 Fall-2017. This repo will be
+updated throughout the 8 week period with exercises.
+
---
+++ exercise/wk2/file3.c
@@ -0,0 +1,20 @@
+/*Debugging quiz - File No: 3 */
+/* The program should give out the appropriate message, given the grade 
+as input. Identify each error and fix it */
+
+#include<stdoi.h>
+
+int main(void) {
+	char input;
+	printf("Enter the grade (A/B/C/D/F): (Q to Quit) \n")
+	while((input == getchar() != 'Q' );{
+		if(input = 'F' | input = 'D'){
+			printf("You have not passed the course.\\n");
+		}
+		elseif(input == 'A' | input == 'B' | input == 'C') {
+			printf("You have passed!\n");
+		}
+	}
+	printf("Quit the menu.\n");
+	return;
+}
