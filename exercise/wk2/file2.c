49fd952b0840eb6ca3be31969e9be217bfa615d8
Your Name <you@example.com>
2017-10-23 11:25:10 -0400

    commits for wk2 & 3

---
+++ exercise/wk2/file2.c
@@ -0,0 +1,13 @@
+/*Debugging quiz - File No: 2 */
+/* The program should correctly print the stored variable values */
+
+include<stdio.h>
+
+int main(){
+	int switch = 3.14;7b = 9;
+	char alphabet = 'k';
+	printf("%Lf\n",&switch)
+	printf("%u",&2b)
+	print("The ASCII value of character 'k' is : %d",'alphabet')
+	return 0;
+}
